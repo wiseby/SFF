ec3ac77938c4db34a4bd6557eaddef3f02288eb1
Lucas Wiseby <l.wiseby@gmail.com>
2020-04-20 12:36:04 +0200

    sql for referencedata added

---
+++ src/Persistence/ReferenceData/Categories.sql
@@ -0,0 +1,14 @@
+-- Categories Seed
+DELETE FROM Category;
+INSERT INTO `Category` (Id, Name, Description)
+VALUES (1, "Horror", "Not for the weak!");
+INSERT INTO `Category` (Id, Name, Description)
+VALUES (2, "Action", "The one and only");
+INSERT INTO `Category` (Id, Name, Description)
+VALUES (3, "Thriller", "Someone is getting murdered");
+INSERT INTO `Category` (Id, Name, Description)
+VALUES (4, "Comedy", "Laugh to the death");
+INSERT INTO `Category` (Id, Name, Description)
+VALUES (5, "Drama", "Dramatic experience increase");
+INSERT INTO `Category` (Id, Name, Description)
+VALUES (6, "Science-Fiction", "Space time!");
---
+++ src/Persistence/ReferenceData/Movies.sql
@@ -0,0 +1,21 @@
+-- SQLite
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("Die Hard", 2, 5, 10, 100.00);
+
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("It", 1, 4, 5, 15.00);
+
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("Lost In Space", 6, 5, 10, 100.00);
+
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("Deadpool", 2, 5, 10, 100.00);
+
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("Along Came Polly", 5, 2, 4, 100.00);
+
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("Morden I Midsommer Season 3", 3, 5, 10, 100.00);
+
+INSERT INTO Movies(Title, CategoryId, Quantity, Licenses, Price)
+VALUES("Dumm Dummare", 4, 5, 10, 100.00);
